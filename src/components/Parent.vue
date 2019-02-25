4a6b7c907b3b6f8f064ab4157e6111e282061c9c
mgz <mgz@lxdeMacBook-Pro.local>
2019-02-25 10:53:05 +0800

    Vuex without actions

--- src/components/Parent.vue
+++ src/components/Parent.vue
@@ -4,12 +4,30 @@
     <button type="button" @click="clickHandler">修改自己文本</button>
     <button type="button" @click="clickHandler2">修改子组件文本</button>
     <div>Test: {{msg}}</div>
-    <child></child>
+    <Child></Child>
   </div>
 </template>
 
 <script>
+import Child from './Child'
 export default {
+  name: 'Parent',
+  components: {
+    Child
+  },
+  computed: {
+    msg () {
+      return this.$store.state.testMsg
+    }
+  },
+  methods: {
+    clickHandler () {
+      this.$store.commit('changeTestMsg', '父组件修改自己后的文本')
+    },
+    clickHandler2 () {
+      this.$store.commit('changeChildText', '父组件修改子组件后的文本')
+    }
+  }
 
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -4,12 +4,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    testMsg: '原始文本',
+    childText: '子组件原始文本'
   },
   mutations: {
-
+    changeTestMsg (state, str) {
+      state.testMsg = str
+    },
+    changeChildText (state, str) {
+      state.childText = str
+    }
   },
   actions: {
-
+    increment(context) {
+      context.commit('changeTestMsg');
+    },
+    decrement(context) {
+      context.commit('changeChildText');
+    }
   }
 })
